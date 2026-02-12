9ab2c3809ff1e35fd3413583476178321c2c2d3c
Shreyansh <shreyanshnayak555@gmail.com>
2026-02-12 21:43:36 +0530

    Update firebase admin to use env variable

--- backend/src/config/firebaseAdmin.js
+++ backend/src/config/firebaseAdmin.js
@@ -1,13 +1,7 @@
 import admin from "firebase-admin";
-import fs from "fs";
-import path from "path";
-import { fileURLToPath } from "url";
-
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = path.dirname(__filename);
 
 const serviceAccount = JSON.parse(
-  fs.readFileSync(path.join(__dirname, "firebase-admin-key.json"), "utf8")
+  process.env.FIREBASE_ADMIN_KEY
 );
 
 admin.initializeApp({
